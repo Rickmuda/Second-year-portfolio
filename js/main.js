cfbbec14643092ed1a5f873d679d4f8d95988e04
MudaIsCarry <Rickambergen25@gmail.com>
2023-10-19 13:55:58 +0200

    temp final

--- js/main.js
+++ js/main.js
@@ -4,3 +4,23 @@ window.addEventListener('load', function () {
         blackScreen.style.display = 'none'; // Hide the black screen when the animation ends
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get references to the small boxes by their IDs
+    const box1 = document.getElementById("box1");
+    const box2 = document.getElementById("box2");
+    const box3 = document.getElementById("box3");
+
+    // Add click event listeners to each small box
+    box1.addEventListener("click", function() {
+        window.location.href = "aboutme.html";
+    });
+
+    box2.addEventListener("click", function() {
+        window.location.href = "projects.html";
+    });
+
+    box3.addEventListener("click", function() {
+        window.location.href = "contact.html";
+    });
+});
