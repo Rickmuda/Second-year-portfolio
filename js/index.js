3ff09e7343f7c3d4af3d9443883df878b13d26e1
MudaIsCarry <Rickambergen25@gmail.com>
2023-11-06 11:23:46 +0100

    yes

--- js/index.js
+++ js/index.js
@@ -17,16 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
             logo.style.transition = 'opacity 1s';
         }
 
-        // After a delay, change the overlay's background to black (screen blackout)
-        setTimeout(function() {
-            overlay.style.transition = 'background 3s';
-            overlay.style.background = 'rgba(0, 0, 0, 1)'; // Change 0.7 to control the opacity
-        }, 1000); // 1000 milliseconds (1 second)
-
         // After another delay, redirect to another page
         setTimeout(function() {
             window.location.href = './html/saves.html'; // Replace with your new page URL
-        }, 5000); // 2000 milliseconds (2 seconds)
+        }, 2000); // 2000 milliseconds (2 seconds)
     }
 
     document.addEventListener('keypress', redirectToNewPage);
--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-window.addEventListener('load', function () {
-    const blackScreen = document.querySelector('.black-screen');
-    blackScreen.addEventListener('animationend', function () {
-        blackScreen.style.display = 'none'; // Hide the black screen when the animation ends
-    });
-});
 
 document.addEventListener("DOMContentLoaded", function() {
     // Get references to the small boxes by their IDs
